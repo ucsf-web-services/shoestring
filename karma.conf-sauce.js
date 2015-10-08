da5d9cc7238361851135b1a7e67213be78ebf586
Richard Baker <richard.baker@ucsf.edu>
2015-10-07 21:23:03 -0700

    testing saucelabs testing

--- karma.conf-sauce.js
+++ karma.conf-sauce.js
@@ -1,96 +1,5 @@
 module.exports = function(config) {
-	var sauceLabsBrowsers = require('./browsers').browsers
-	var customLaunchers = {
-		/* WINDOWS BROWSERS */
-
-		// Internet Explorer
-		sl_ie8_win7: {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			version: '8',
-			platform: 'windows 7'
-		},
-
-		sl_ie9_win7: {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			version: '9',
-			platform: 'windows 7'
-		},
-
-		sl_ie10_win8: {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			version: '10',
-			platform: 'windows 8'
-		},
-
-		sl_ie11_win81: {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			version: '11',
-			platform: 'windows 8.1'
-		},
-
-		// chrome
-
-		sl_chrome_win81: {
-			base: 'SauceLabs',
-			browserName: 'chrome',
-			platform: 'windows 8.1'
-		},	
-
-		// firefox
-		sl_ie_win7: {
-			base: 'SauceLabs',
-			browserName: 'Firefox',
-			platform: 'windows 8.1'
-		},	
-
-		/* OSX */
-
-		sl_osx_safari: {
-			base: 'SauceLabs',
-			browserName: 'safari',
-			platform: 'OS X 10.10'
-		},
-
-		sl_osx_chrome: {
-			base: 'SauceLabs',
-			browserName: 'chrome',
-			platform: 'OS X 10.10'
-		},
-
-		sl_osx_firefox: {
-			base: 'SauceLabs',
-			browserName: 'firefox',
-			platform: 'OS X 10.10'
-		},
-
-		/* IPHONE */
-
-		sl_iphone_safari: {
-			base: 'SauceLabs',
-			browserName: "iphone",
-			platform: 'OS X 10.10',
-			version: "8.2"
-		},
-
-		/* LINUX */
-
-		sl_linux_chrome: {
-			base: 'SauceLabs',
-			browserName: 'chrome',
-			platform: 'Linux'
-		},
-		
-		sl_linux_firefox: {
-			base: 'SauceLabs',
-			browserName: 'firefox',
-			platform: 'Linux'
-		}
-	}
-
+	var sauceLabsBrowsers = require('./browsers.json').browsers
 	config.set({
 		frameworks: ['jasmine'],
 		files: [
@@ -107,6 +16,6 @@ module.exports = function(config) {
       		recordScreenshots: false
 		},
 		customLaunchers: sauceLabsBrowsers,
-		browsers: Object.keys(customLaunchers)
+		browsers: Object.keys(sauceLabsBrowsers)
 	});
 };
